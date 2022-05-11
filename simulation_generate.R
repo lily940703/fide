a5602f54fb5cc0b5d5979e60e07fca2f44d3834a
lily940703 <mail@lily940703.com>
2022-05-11 10:41:50 +0800

    simulation data

--- simulation_generate.R
+++ simulation_generate.R
@@ -40,7 +40,40 @@ for (i in 1:length(IDI)) {
   }
 }
 
+dataset_simulation = list(dataset_84, dataset_108, dataset_132, dataset_156)
+dataset_simulation_test = list()
+dataset_simulation_train = list()
 
+for (i in 1:4) {
+  for (j in 1:16000) {
+    lentry_test = list()
+    lentry_train = list()
+
+    lentry_test$tsid = lentry_train$tsid = tsid = (i-1)*16000+j
+    lentry_test$h = lentry_train$h = h = 12
+    lentry_test$length = len_test = dim(dataset_simulation[[i]])[2]-h
+    lentry_train$length = len_train = len_test-h
+
+    x1 = dataset_simulation[[i]][j,][1:len_test]
+    x1 <- data.frame(x1, c(1:length(x1)))
+    starting_period <- min(x1[x1$x1>0,2])
+    x1_new <- x1$x1[starting_period:nrow(x1)]
+    lentry_test$x = x = ts(x1_new)
+    lentry_test$xx = ts(dataset_simulation[[i]][j,][(len_test+1):(len_test+12)])
+    lentry_test$IDI = compute_ADI(x)
+    lentry_test$cv2 = compute_CV2(x)
+
+    lentry_train$x = ts(x[1:(length(x)-h)])
+    lentry_train$xx = ts(x[(length(x)-h+1):length(x)])
+    lentry_train$IDI = compute_ADI(lentry_train$x)
+    lentry_train$cv2 = compute_CV2(lentry_train$x)
+
+    dataset_simulation_train[[tsid]] = list()
+    dataset_simulation_train[[tsid]] = lentry_train
+    dataset_simulation_test[[tsid]] = list()
+    dataset_simulation_test[[tsid]] = lentry_test
+  }
+}
 
 # Plot the simulated text set
 idi = c()
